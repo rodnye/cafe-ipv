976262ab4e8137693e6c83f3b4905b0d252e55dd
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-03-10 06:15:05 -0400

    chore: add capacitor config and deps

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.rodnye.cafeipv',
+  appName: 'Mycafe IPV',
+  webDir: 'dist'
+};
+
+export default config;
