6c38705962be9fff5ba04da541f791cf0ca2551e
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-03-13 07:46:40 -0400

    ci: add deploy.yml for automated deployment on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: process.env.NODE_ENV === 'github-pages' ? '/cafe-ipv' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
