7c1c462791a91c74722ac9500a7a9652e6bac4ae
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-03-20 01:48:44 -0400

    feat: generate app icons and splash screens using @capacitor/assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.github.rodnye.cafeipv',
   appName: 'Mycafe IPV',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
