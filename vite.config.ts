47396bad5f66d9ffcc4766fdc54e0dd09ba019f3
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-03-20 01:49:38 -0400

    feat: add PWA support with manifest and service worker

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      manifest: false, // located in /public folder
+    }),
+  ],
   base: process.env.BASE_URL || '/',
   resolve: {
     alias: {
